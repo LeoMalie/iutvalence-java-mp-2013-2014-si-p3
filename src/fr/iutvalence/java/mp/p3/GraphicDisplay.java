452a945818615b4c914fe28c1cf185b06ea0f2e8
LeoMalie <leomalie@yahoo.fr>
2013-11-29 09:55:16 +0100

    Created GUI

--- src/fr/iutvalence/java/mp/p3/GraphicDisplay.java
+++ src/fr/iutvalence/java/mp/p3/GraphicDisplay.java
@@ -1,13 +1,47 @@
 package fr.iutvalence.java.mp.p3;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * This class provides to get a GUI
  * @author maliel
  *
  */
+@SuppressWarnings("serial")
 public class GraphicDisplay extends JFrame implements Display
 {
+    private JLabel[][] labels;
+    
+    private JLabel score;
+    
+    public static final int SIZE_LAYOUT = 500;
+    
+    public void GraphicDisplay()
+    {
+        this.setTitle("CrashCar V1.0");
+        this.setSize(SIZE_LAYOUT, SIZE_LAYOUT);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GridLayout gl = new GridLayout(SIZE_LAYOUT, SIZE_LAYOUT);
+        gl.preferredLayoutSize(this);
+        
+        JPanel panelZone = new JPanel();
+        panelZone.setLayout(gl);
+
+        
+        
+        JPanel pan = new JPanel();
+        
+        this.setContentPane(pan);               
+        this.setVisible(true);
+    }
+    
     
 }
